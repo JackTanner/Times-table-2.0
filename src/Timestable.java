317e7dcb2ed66d048f1d577f3f989253e5a6833b
JackTanner <Tanner.mcculloch-cop@ocsbstudent.ca>
2017-10-13 14:38:16 -0400

    save 1.0

---
+++ src/Timestable.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+import java.util.Scanner;
+public class Timestable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in=new Scanner(System.in);
+		Random rnd=new Random();
+		int hi;
+		int low;
+		int low2;
+		int dif;
+		int y=1;
+		System.out.println("Please give me your highest number you want to work with.");
+		hi=in.nextInt();
+		System.out.println("Please give me your lowest number you want to work with.");
+		low=in.nextInt();
+		low2=low;
+		dif=hi-low;
+		for (int x=low;x<=hi;x++) {
+			System.out.print("\t"+x+"\t");
+		}  	System.out.println();
+		System.out.print(low);
+		for (int x=dif;x<=hi;x++) {
+			System.out.print(" \t"+(low*(low)));
+			
+		}
+
+	}
+}
